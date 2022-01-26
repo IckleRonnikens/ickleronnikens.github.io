bfb27097e672d0c389be0d603320c99316bc4651
IckleRonnikens <76468847+IckleRonnikens@users.noreply.github.com>
2022-01-27 04:31:51 +1100

    Site updated: 2022-01-27 04:31:45

---
+++ js/search.js
@@ -0,0 +1,8 @@
+var monkeyList = new List('post-list', { 
+    valueNames: ['name', 'title', 'image', 'author', 'slug', 'category', 'coverart', 'artwork', 'summary', 'tags'],
+    page: 15,
+    pagination: true,
+    
+  }
+  
+  );
