f7b28a859989a7560261db793af2288b31ac29c0
IckleRonnikens <76468847+IckleRonnikens@users.noreply.github.com>
2022-06-11 03:48:24 +1000

    Delete js directory

--- js/search.js
+++
@@ -1,8 +0,0 @@
-var monkeyList = new List('post-list', { 
-    valueNames: ['name', 'title', 'image', 'author', 'slug', 'category', 'coverart', 'artwork', 'summary', 'tags'],
-    page: 15,
-    pagination: true,
-    
-  }
-  
-  );
